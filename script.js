29dcf165099e589a144e2db5b0975739fc85af07
Alessandra <alessandra.wilsmann1@gmail.com>
2026-03-09 21:07:31 -0300

    melhorias na aula

--- script.js
+++ script.js
@@ -20,10 +20,16 @@ function calcularTotal() {
 
     });
 
+     let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+
+    carrinho.forEach(produto => {
+        totalItens += Number(produto.quantidade);
+    });
+
     document.getElementById("valor-total").textContent =
         total.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
-        document.getElementById("contador-carrinho").textContent = totalItens;
 
+    document.getElementById("contador-carrinho").textContent = totalItens;
 }
 
 
@@ -84,3 +90,36 @@ async function carregarDepoimentos() {
 }
 
 carregarDepoimentos();
+
+
+
+function adicionarCarrinho(nome, preco, descricao, id){
+   
+    const quantidade = document.getElementById("produto_"+id);
+    const valorquantidade = quantidade.value;
+    let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+    carrinho.push({
+        nome: nome,
+        preco: preco,
+        descricao: descricao,
+        quantidade: Number(valorquantidade)
+    });
+
+    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+    atualizarContadorCarrinho();
+
+}
+
+function atualizarContadorCarrinho() {
+
+    let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+
+    let totalItens = 0;
+
+    carrinho.forEach(produto => {
+        totalItens += Number(produto.quantidade);
+    });
+
+    document.getElementById("contador-carrinho").textContent = totalItens;
+}
+atualizarContadorCarrinho();
